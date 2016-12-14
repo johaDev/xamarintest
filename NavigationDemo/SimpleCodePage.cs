c7f16404dded075b512d2f556bc6f65a299c47d1
Josef Haslinger <josef.haslinger@cgm.com>
2016-12-14 22:45:51 +0100

    working

--- NavigationDemo/SimpleCodePage.cs
+++ NavigationDemo/SimpleCodePage.cs
@@ -8,15 +8,22 @@ namespace NavigationDemo
 	{
 		public SimpleCodePage()
 		{
+
+			var picker = new Picker { Title = "Pick a fruit" };
+			picker.Items.Add("Apple");
+			picker.Items.Add("Orange");
+			picker.Items.Add("Banana");
+
 			Content = new StackLayout
 			{
 				Children = {
 					new Label { Text = "A Page written in C#." },
 					new Button { Text = "OK" },
 					new SearchBar {  Placeholder ="enter text" },
-					new Picker { Title ="Title"}
+					picker
 				}
 			};
+
 		}
 	}
 }
--- NavigationDemo/SimpleXamlPage.xaml.cs
+++ NavigationDemo/SimpleXamlPage.xaml.cs
@@ -1,15 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 using Xamarin.Forms;
 
 namespace NavigationDemo
 {
-	public partial class SimpleXamlPage : ContentPage
+	public partial class SimpleXamlPage : ContentPage  /*, INotifyPropertyChanged */
 	{
 		public SimpleXamlPage()
 		{
 			InitializeComponent();
+
+			BindingContext = this;
 		}
+
+		public string LabelText { get; set; } = "My Label-title from the Code";
+
+		private string _firstName = "Donald";
+		public string FirstName {
+			get {
+				return _firstName;
+			}
+			set {
+				_firstName = value;
+				OnPropertyChanged(nameof(FirstName));
+			}
+		}
+
+		/*
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected virtual void OnPropertyChanged(string propName)
+		{
+			var changed = PropertyChanged;
+			if (changed != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(propName));
+			}
+		}
+*/
 	}
 }
